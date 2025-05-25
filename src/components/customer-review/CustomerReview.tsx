96d63eaf8077088367ee01c586d1d63af4379ff2
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2025-05-25 11:27:40 +0600

    add

---
+++ src/components/customer-review/CustomerReview.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React from "react";
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import { FaStar } from "react-icons/fa6";
+import { useGetReviewQuery } from "@/redux/features/api/reviewApi";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+const CustomerReview = () => {
+  const { data: reviews = [], isLoading } = useGetReviewQuery();
+
+
+
+
+  if (isLoading) return <p>Loading…</p>;
+
+  return (
+    <MaxWidthWrapper>
+      <h2 className="text-2xl font-semibold text-center my-5 text-gray-800">
+        Customer Reviews
+      </h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-5 relative">
+        <Carousel >
+          <CarouselContent>
+            {reviews.map((review) => (
+              <CarouselItem key={review.id}>
+                <div className="p-4 rounded-md border border-gray-300 shadow-sm hover:shadow-md transition">
+                  {/* Name */}
+                  <h3 className="text-lg font-semibold text-gray-800 mt-2">
+                    {review.name}
+                  </h3>
+
+                  {/* Star rating */}
+                  <p className="text-green-500 flex gap-1">
+                    {Array.from({ length: review.rating }, (_, i) => (
+                      <FaStar key={i} size={20} />
+                    ))}
+                  </p>
+
+                  {/* Comment */}
+                  <p className="text-sm font-normal text-gray-600 mt-2">
+                    {review.comment}
+                  </p>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+           <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+  
+      </div>
+    </MaxWidthWrapper>
+  );
+};
+
+export default CustomerReview;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import MaxWidthWrapper from '../MaxWidthWrapper'
+
+
+import logo2 from '/public/logo/logo2.jpg';
+import Image from 'next/image';
+import Link from 'next/link';
+import { SiShopify } from 'react-icons/si';
+import { HiMenuAlt1 } from 'react-icons/hi';
+
+
+const Header = () => {
+  return (
+    <div className='bg-gradient-to-l from-[var(--gradient-from)] to-[var(--gradient-to)] '>
+
+    <MaxWidthWrapper>
+
+        <div className='flex justify-between gap-3 items-center   h-20'>
+          
+   
+            <div className=''> 
+                <button className='text-[#FFA725] cursor-pointer'><HiMenuAlt1    size={40} /></button>
+          
+            </div>
+
+            <Link href="#" className='flex items-center gap-2'>
+                <Image src={logo2} width={100} height={100} alt='logo' className='w-[100px] h-[80px]'/>
+           
+            </Link>
+       
+            <div className='relative'> 
+                <button className='text-[#FFA725] cursor-pointer'><SiShopify  size={40} /></button>
+                <span className='text-xs font-normal bg-[#FFA725] text-white p-0.5 rounded-full absolute -top-2 -right-1'>02</span>
+            </div>
+
+        </div>
+    </MaxWidthWrapper>
+    </div>
+  )
+}
+
+export default Header
